61272bb7dd91b61eeaf0660a4839d87295310a55
Ahmed Tarek <7madak10@gmail.com>
2024-12-18 18:04:20 +0200

    Added intro screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 import 'app_state.dart';
@@ -71,15 +72,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final bool isLoggedIn = Provider.of<ApplicationState>(context).loggedIn;
-    final List<Widget> pages = [
-      EventsPage(
-        userUid: isLoggedIn ? FirebaseAuth.instance.currentUser!.uid : null,
-      ),
-      Friends(),
-      Card(),
-      InGifts(),
-      OutGifts(),
-    ];
+    List<Widget> pages = [];
+    if (isLoggedIn) {
+      pages = [
+        EventsPage(
+          userUid: isLoggedIn ? FirebaseAuth.instance.currentUser!.uid : null,
+        ),
+        Friends(),
+        Card(),
+        InGifts(),
+        OutGifts(),
+      ];
+    }
+
     var theme = Theme.of(context);
     var style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.primary,
@@ -93,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   _selectedIdx == 4
               ? pages[_selectedIdx]
               : pages[1]
-          : const SignInWidget(),
+          : const IntroductionScreenPage(),
       bottomNavigationBar: isLoggedIn
           ? NavigationBar(
               destinations: const [
@@ -128,7 +133,53 @@ final _router = GoRouter(
         GoRoute(
           path: 'sign-in',
           builder: (context, state) {
-            return const SignInWidget();
+            return SignInScreen(
+              actions: [
+                ForgotPasswordAction(((context, email) {
+                  final uri = Uri(
+                    path: '/sign-in/forgot-password',
+                    queryParameters: <String, String?>{
+                      'email': email,
+                    },
+                  );
+                  context.push(uri.toString());
+                })),
+                AuthStateChangeAction(((context, state) async {
+                  final user = switch (state) {
+                    SignedIn state => state.user,
+                    UserCreated state => state.credential.user,
+                    _ => null
+                  };
+                  if (user == null) {
+                    return;
+                  }
+                  if (state is UserCreated) {
+                    user.updateDisplayName(user.email!.split('@')[0]);
+                    // Add the new user to the Firestore database
+                    final userDocRef = FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(user.uid);
+
+                    await userDocRef.set({
+                      'name': user.displayName ?? user.email!.split('@')[0],
+                      'email': user.email ?? '',
+                      'phoneNumber': user.phoneNumber ?? '',
+                      'friends': [],
+                      'events': [],
+                      'createdAt': FieldValue.serverTimestamp(),
+                    });
+                  }
+                  if (!user.emailVerified) {
+                    user.sendEmailVerification();
+                    const snackBar = SnackBar(
+                        content: Text(
+                            'Please check your email to verify your email address'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                  context.pushReplacement('/');
+                })),
+              ],
+            );
           },
           routes: [
             GoRoute(
@@ -155,8 +206,9 @@ final _router = GoRouter(
                 DisplayNameChangedAction((context, oldName, newName) {
                   //update users document with the new name
                   final user = FirebaseAuth.instance.currentUser;
-                  final userDocRef =
-                      FirebaseFirestore.instance.collection('users').doc(user!.uid);
+                  final userDocRef = FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(user!.uid);
                   userDocRef.update({'name': newName});
                 }),
               ],
@@ -169,58 +221,136 @@ final _router = GoRouter(
   ],
 );
 
-class SignInWidget extends StatelessWidget {
-  const SignInWidget({
-    super.key,
-  });
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SignInScreen(
-      actions: [
-        ForgotPasswordAction(((context, email) {
-          final uri = Uri(
-            path: '/sign-in/forgot-password',
-            queryParameters: <String, String?>{
-              'email': email,
-            },
-          );
-          context.push(uri.toString());
-        })),
-        AuthStateChangeAction(((context, state) async {
-          final user = switch (state) {
-            SignedIn state => state.user,
-            UserCreated state => state.credential.user,
-            _ => null
-          };
-          if (user == null) {
-            return;
-          }
-          if (state is UserCreated) {
-            user.updateDisplayName(user.email!.split('@')[0]);
-            // Add the new user to the Firestore database
-            final userDocRef =
-                FirebaseFirestore.instance.collection('users').doc(user.uid);
-
-            await userDocRef.set({
-              'name': user.displayName ?? user.email!.split('@')[0],
-              'email': user.email ?? '',
-              'phoneNumber': user.phoneNumber ?? '',
-              'friends': [],
-              'events': [],
-              'createdAt': FieldValue.serverTimestamp(),
-            });
-          }
-          if (!user.emailVerified) {
-            user.sendEmailVerification();
-            const snackBar = SnackBar(
-                content: Text(
-                    'Please check your email to verify your email address'));
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          }
-          context.pushReplacement('/');
-        })),
-      ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to Celebratio'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  context.go('/sign-in');
+                },
+                child: Text('Get Started')),
+          ],
+        ),
+      ),
     );
   }
+}
+
+class IntroductionScreenPage extends StatefulWidget {
+  const IntroductionScreenPage({super.key});
+
+  @override
+  State<IntroductionScreenPage> createState() => _IntroductionScreenPageState();
+}
+
+class _IntroductionScreenPageState extends State<IntroductionScreenPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(BuildContext context) {
+    context.go('/sign-in'); // Navigate to sign-in page when intro ends
+  }
+
+  Widget _buildImage(String assetName) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(250.0),
+      // Adjust this value for roundness
+      child: Image.asset(
+        'asset/images/$assetName',
+        width: 350,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: TextStyle(fontSize: 19.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      pages: [
+        PageViewModel(
+          title: "Welcome to Celebratio!",
+          bodyWidget: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(250.0),
+                child: Image.asset(
+                  'asset/images/Celebration.png',
+                  width: 350,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 20), // Space between image and body
+              const Text(
+                "Discover the joy of celebrating with friends.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 19.0),
+              ),
+            ],
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Connect with Friends",
+          bodyWidget: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(250.0),
+                child: Image.asset(
+                  'asset/images/Friends.png',
+                  width: 350,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Easily manage and share events with your close ones.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 19.0),
+              ),
+            ],
+          ),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      onSkip: () => _onIntroEnd(context),
+      showSkipButton: true,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text(
+        "Get Started",
+        style: TextStyle(fontWeight: FontWeight.w600),
+      ),
+      dotsDecorator: DotsDecorator(
+        size: const Size.square(10.0),
+        activeSize: const Size(20.0, 10.0),
+        activeColor: Theme.of(context).primaryColor,
+        color: Colors.black26,
+        spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+      ),
+    );
+  }
+
 }
